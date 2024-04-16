0bc622654173559109ada3daadc09de1a7adac0c
Jefferson <jefferson@educationjourney.com>
2024-04-16 18:33:10 -0300

    feat: Fetching User Input

--- src/app.ts
+++ src/app.ts
@@ -44,10 +44,38 @@ class ProjectInput {
         this.attach();
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;    
+
+        if(
+            !enteredTitle.trim() ||
+            !enteredDescription.trim() ||
+            !enteredPeople.trim()
+        ){
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
     @autoBind
     private submitHandler(event: Event){
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if(Array.isArray(userInput)){
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+            this.clearInputs();
+        }
     }
     private configure(){
         this.element.addEventListener('submit', this.submitHandler);
