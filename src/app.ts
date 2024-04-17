2c6d6ed8d29912cce6c41b72e0c5d185c59f28f1
Jefferson <jefferson@educationjourney.com>
2024-04-17 00:12:28 -0300

    feat: Rendering Project Lists

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,36 @@ function autoBind(_target: any, _key: string, descriptor: PropertyDescriptor) {
     return boundDescriptor;
 }
 
+class ProjectList {
+    templateElement : HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById(
+            'project-list'
+        )! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        
+        const importeNode = document.importNode(
+            this.templateElement.content, true
+        )
+        this.element = importeNode.firstElementChild as HTMLFormElement;
+        this.element.id = `${this.type}-projects`;
+
+        this.attach();
+        this.renderContent();
+    }
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
+
 class ProjectInput {
     templateElement : HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -141,3 +171,5 @@ class ProjectInput {
 }
 
 const renderProjectInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
