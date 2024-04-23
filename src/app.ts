f317deff052bddae77609bd02ea449006a9c4398
Jefferson <jefferson@educationjourney.com>
2024-04-23 20:12:47 -0300

    feat: Adding a Droppable Area

--- src/app.ts
+++ src/app.ts
@@ -154,11 +154,12 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements 
     
     @autoBind
     dragStartHandler(event: DragEvent) {
-        console.log(event);
+        event.dataTransfer!.setData('text/plain', this.project.id);
+        event.dataTransfer!.effectAllowed = 'move';
     }
 
-    dragEndHandler(_: DragEvent) {
-        console.log('dragEnd');
+    dragEndHandler(event: DragEvent) {
+        console.log(event.dataTransfer!.getData('text/plain'));
     }
 
     configure() {
@@ -185,13 +186,16 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
     }
 
     @autoBind
-    dragOverHandler(_: DragEvent) {
-        const listEl = this.element.querySelector('ul')!;
-        listEl.classList.add('droppable');
+    dragOverHandler(event: DragEvent) {
+        if(event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault();
+            const listEl = this.element.querySelector('ul')!;
+            listEl.classList.add('droppable');
+        }
     }
 
-    dropHandler(_: DragEvent) {
-        
+    dropHandler(event: DragEvent) {
+        console.log(event.dataTransfer!.getData('text/plain'));
     }
 
     @autoBind
