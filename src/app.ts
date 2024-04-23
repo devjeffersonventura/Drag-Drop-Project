df743d0b4c49a32dbeff53096d65b9d6f4951aa8
Jefferson <jefferson@educationjourney.com>
2024-04-23 20:22:36 -0300

    feat: Finishing Drag & Drop

--- src/app.ts
+++ src/app.ts
@@ -49,6 +49,18 @@ class ProjectState extends State<Project> {
     addProject(title: string, description: string, numOfPeople: number) {
         const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);
         this.projects.push(newProject);
+        this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if(project && project.status !== newStatus) {
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+
+    private updateListeners() {
         for(const listenerFn of this.listener) {
             listenerFn(this.projects.slice());
         }
@@ -194,8 +206,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
         }
     }
 
+    @autoBind
     dropHandler(event: DragEvent) {
-        console.log(event.dataTransfer!.getData('text/plain'));
+        const prjId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
     }
 
     @autoBind
